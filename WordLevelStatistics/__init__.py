5485b4d04441e6e0afef87e33ad8f0c80cbc3078
Shubhanshu Mishra <smishra8@illinois.edu>
2014-12-29 20:40:35 -0600

    Updated package details

--- WordLevelStatistics/__init__.py
+++ WordLevelStatistics/__init__.py
@@ -1,3 +1,5 @@
 """
 Hello World
 """
+
+from .WordLevelStatistics import WordLevelStatistics
--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 setup(name='WordLevelStatistics',
-            version='0.1',
-            description='Implementation of WordLevelStatistics module from perl',
-            url='http://github.com/napsternxg/WordLevelStatistics',
-            author='Shubhanshu Mishra',
-            author_email='smishra8@illinois.edu',
-            license='GPL v2.0',
-            packages=['WordLevelStatistics'],
-            zip_safe=False)
+    version='0.1',
+    description='Implementation of WordLevelStatistics module from perl',
+    long_description=readme(),
+    url='http://github.com/napsternxg/WordLevelStatistics',
+    author='Shubhanshu Mishra',
+    author_email='smishra8@illinois.edu',
+    clasifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License v2
+        (GPLv2)",
+        "Topic :: Text Processing :: General"
+    ]
+    license='GPL v2.0',
+    packages=['WordLevelStatistics'],
+    include_package_data=True,
+    zip_safe=False)
