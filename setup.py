d4ae838424a0eee17b4726ccf6957ec1670fa6c9
Shubhanshu Mishra <smishra8@illinois.edu>
2014-12-29 20:25:27 -0600

    Created Module.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='WordLevelStatistics',
+            version='0.1',
+            description='Implementation of WordLevelStatistics module from perl',
+            url='http://github.com/napsternxg/WordLevelStatistics',
+            author='Shubhanshu Mishra',
+            author_email='smishra8@illinois.edu',
+            license='GPL v2.0',
+            packages=['WordLevelStatistics'],
+            zip_safe=False)
